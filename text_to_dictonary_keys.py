da7e6ebf65774df5a9f86ff1529e3393e2e2e1a8
Roy Contreras Benitez <roy7474@gmail.com>
2023-08-12 18:24:21 -0400

    last updates and final testing to the program have been done

--- text_to_dictonary_keys.py
+++ text_to_dictonary_keys.py
@@ -2,17 +2,15 @@
 # tfile = input('Enter the name of the file you would like to open: ')
 
 words_count = {}
-fhandle = open('words.txt')
+fhandle = open('words.txt')             # open the file
 
 for line in fhandle:
-    words = line.split()
-#    words_count[line]= words_count.get(line,0) + 1
-#    word = lines.split()
+    words = line.split()                # split the text into words
+#    words_count[words]= words_count.get(words,0) + 1
     for word in words:
-        if word not in words_count:
+        if word not in words_count:     #if the key word is not already in the dictionary. add it and add a value of 1
             words_count[word] = 1
-        else:
+        else:                           # else, add 1 to the key word
             words_count[word] += 1
 
 print(words_count)
-        
